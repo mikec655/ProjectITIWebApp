7476623248a0e22e24053713c78f98ece2f21b8b
Youri <youri.sanders@hotmail.com>
2019-01-22 19:09:31 +0100

    marker

--- openmap.php
+++ openmap.php
@@ -8,8 +8,8 @@
 
             var lonLat = new OpenLayers.LonLat(77.216721, 28.644800)
                     .transform(
-                            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-                            map.getProjectionObject() // to Spherical Mercator Projection
+                            new OpenLayers.Projection("EPSG:4326"),
+                            map.getProjectionObject()
                             );
 
             var zoom = 5;
@@ -27,62 +27,14 @@
             while (i < 10) {
                 var newlonLat = new OpenLayers.LonLat(lon, lat)
                         .transform(
-                                new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-                                map.getProjectionObject() // to Spherical Mercator Projection
+                                new OpenLayers.Projection("EPSG:4326"),
+                                map.getProjectionObject()
                                 );
                 markers.addMarker(new OpenLayers.Marker(newlonLat));
                 i++;
-            var lon =+ lon + 1;
-            var lat =+ lat + 1;
+                var lon = +lon + 1;
+                var lat = +lat + 1;
             }
-
-
-
-
-
-//            /// marker popup
-//
-//
-//            var map;
-//            function init() {
-//
-//                // The overlay layer for our marker, with a simple diamond as symbol
-//                var overlay = new OpenLayers.Layer.Vector('Overlay', {
-//                    styleMap: new OpenLayers.StyleMap({
-//                        externalGraphic: '../img/marker.png',
-//                        graphicWidth: 20, graphicHeight: 24, graphicYOffset: -24,
-//                        title: '${tooltip}'
-//                    })
-//                });
-//
-//                // The location of our marker and popup. We usually think in geographic
-//                // coordinates ('EPSG:4326'), but the map is projected ('EPSG:3857').
-//                var myLocation = new OpenLayers.Geometry.Point(10.2, 48.9)
-//                        .transform('EPSG:4326', 'EPSG:3857');
-//
-//                // We add the marker with a tooltip text to the overlay
-//                overlay.addFeatures([
-//                    new OpenLayers.Feature.Vector(myLocation, {tooltip: 'OpenLayers'})
-//                ]);
-//
-//                // A popup with some information about our location
-//                var popup = new OpenLayers.Popup.FramedCloud("Popup",
-//                        myLocation.getBounds().getCenterLonLat(), null,
-//                        '<a target="_blank" href="http://openlayers.org/">We</a> ' +
-//                        'could be here.<br>Or elsewhere.', null,
-//                        true // <-- true if we want a close (X) button, false otherwise
-//                        );
-//
-//                // Finally we create the map
-//                map = new OpenLayers.Map({
-//                    div: "map", projection: "EPSG:3857",
-//                    layers: [new OpenLayers.Layer.OSM(), overlay],
-//                    center: myLocation.getBounds().getCenterLonLat(), zoom: 15
-//                });
-//                // and add the popup to it.
-//                map.addPopup(popup);
-//            }
-
         </script>
     </body>
 </html>

452217a4ef80469548977da819c3c92094548df6
Youri <youri.sanders@hotmail.com>
2019-01-23 15:52:46 +0100

    Changed login page text and did some more styling via css

--- Login.php
+++ Login.php
@@ -7,11 +7,11 @@
     <body> 
         <div class="filler"></div>
             <div class="container">
-                    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off"><link rel="stylesheet" type="text/css" href="youri.css">
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off"><link rel="stylesheet" type="text/css" href="style/style.css">
                         <ul class="loginstyling">    
                             <img src="">
                             <br>
-                            <p>Welkom op Sonimod hier kunt u inloggen.<p>
+                            <p>Welcome to the weatherdata portal.<p>
                             <input type="text" name="name" class="contactstyling" placeholder="Vul uw inlognaam in" value="" maxlength="40" />
                             </br></br>
                             <input type="password" name="pass" class="form-control" placeholder="Vul uw wachtwoord in" maxlength="15" />
--- footermodule.php
+++ footermodule.php
@@ -1,3 +1,5 @@
 <html>
-    <font color=white><&copy;ITV2AGROUP1></font>
+    <div class="footer">
+        <font color=white>&copy;ITV2AGROUP1</font>
+    </div>
 </html>

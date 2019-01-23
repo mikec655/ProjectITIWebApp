bc901bcf3fb686efff7b3e47cbed659f3a82461d
Youri <youri.sanders@hotmail.com>
2019-01-23 15:40:21 +0100

    Basic login page without functionality

---
+++ Login.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+    <?php
+        include "headermodule.php";
+    ?>
+    </head>
+    <body> 
+        <div class="filler"></div>
+            <div class="container">
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off"><link rel="stylesheet" type="text/css" href="youri.css">
+                        <ul class="loginstyling">    
+                            <img src="">
+                            <br>
+                            <p>Welkom op Sonimod hier kunt u inloggen.<p>
+                            <input type="text" name="name" class="contactstyling" placeholder="Vul uw inlognaam in" value="" maxlength="40" />
+                            </br></br>
+                            <input type="password" name="pass" class="form-control" placeholder="Vul uw wachtwoord in" maxlength="15" />
+                            </br></br>
+                            <input type="submit" class= "btn btn-block btn-primary" name="btn-login" value="Inloggen"/>
+                        </ul>
+                    </form>     
+            </div>
+        
+        
+        <div id="footer">
+            <div class="container">
+                <?php
+                include 'footermodule.php';
+                ?>
+            </div>
+        </div>
+</html>

0a2352f10e53a72b254875ab2985e19f5154ee8a
Youri <Youri.sanders@hotmail.com>
2019-01-24 13:55:23 +0100

    hashed passwords in post

--- inc/loginscript.php
+++ inc/loginscript.php
@@ -1,8 +1,17 @@
 <?php
-
 defined('DS') OR die('No direct access allowed.');
+
+$username = 'user';
+$password = 'userpass';
+
+$random1 = 'secret_key1';
+$random2 = 'secret_key2';
+
+$hash = md5($random1.$password.$random2); 
+
+
 $users = array(
-    "user" => "userpass"
+    "$username" => "$hash"
 );
 
 if (isset($_GET['logout'])) {
@@ -11,7 +20,7 @@ if (isset($_GET['logout'])) {
 }
 
 if (isset($_POST['username'])) {
-    if ($users[$_POST['username']] !== NULL && $users[$_POST['username']] == $_POST['password']) {
+    if ($hash == $hash && $users[$_POST['username']] !== NULL && $users[$_POST['username']] == md5($random1.$_POST['password'].$random2)) {
         $_SESSION['username'] = $_POST['username'];
         header('Location:  ' . $_SERVER['PHP_SELF']);
     } else {

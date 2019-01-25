53c14beb8974d4663100db9f4df77f95153ae836
Kevin <kevinbeckwee@gmail.com>
2019-01-25 20:42:15 +0100

    Formatting for easier data reading

--- DataGrafiek/Grafiek.php
+++ DataGrafiek/Grafiek.php
@@ -6,7 +6,7 @@
 <?php
 include_once("inc/dataReader.php");
 $station = readDataOfStation("2019-01-21",'100050' , "11000001000", 60, FALSE);
-//echo $station;
+
 /*foreach($station as $test){
     print_r($test);
     echo '<br/>';
@@ -15,7 +15,16 @@ for($i = 0; $i < count($station['time']); $i++){
     $time = $station['time'][$i];
     $temp = round($station['temp'][$i], 1);
     $prcp = round($station['prcp'][$i], 2);
-    echo "<tr><th>$time</th><br/><td>$temp</td><br/><td>$prcp</td></tr>";
+    echo "<tr>" .
+        "Time: ".
+        "<th>$time</th>" .
+        "<br/>" .
+        "Temp: ".
+        "<td>$temp</td>" .
+        "<br/>" .
+        "Prcp: ".
+        "<td>$prcp</td>" .
+        "</tr><br/><br/>";
 }
 ?>
 

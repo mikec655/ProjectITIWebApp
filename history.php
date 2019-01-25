f26cd28dcac3f4e8d5a6839d290ac23ce5c7846b
Mike Clarke <mikec655@gmail.com>
2019-01-25 11:43:31 +0100

    PHP code for history page w/ GET

--- history.php
+++ history.php
@@ -8,9 +8,12 @@
     </head>
     <?php include ("inc/popup.php"); ?>
     <body>
-        <div style="background-color:black;">
-            <div style=float:left style="background-color:green;">
-                <b>stations</b>
+        <center>
+        
+        <h1>Weather Data History of Station <?php echo $_GET['station'] ?></h1>
+        <div>
+            <div>
+                <b>Stations</b>
                 <br>
                 <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
                 <br>
@@ -26,7 +29,7 @@
             $dateMin28 = $dateMin28->sub(new DateInterval('P28D'));
             $dateMin28 = $dateMin28->format('Y-m-d')
             ?>
-            <div style=float:left>
+            <div>
                 <form action= "history.php">
                     <b>Agenda</b>
                     <br>
@@ -38,22 +41,18 @@
                 </form>
             </div>
         </div>
-        <br>
-
-    <center>
         <table>
-            <tr><th></th><th><h1>Weather data from today</h1><br>
-            <tr><th></th><th>Weather Station</th><th>Country</th><th>Heat Index</th></tr>
-            <tr><th>1</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>2</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>3</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>4</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>5</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>6</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>7</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>8</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>9</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-            <tr><th>10</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
+            <tr><th>Time</th><th>Temperature</th><th>Rainfall</th></tr>
+            <?php 
+                include("inc/dataReader.php");
+                $station = readDataOfStation($_GET['date'], $_GET['station'], "11000001000", 60, FALSE);
+                for($i = 0; $i < count($station['time']); $i++){
+                    $time = $station['time'][$i];
+                    $temp = $station['temp'][$i];
+                    $prcp = $station['prcp'][$i];
+                    echo "<tr><th>$time</th><td>$temp</td><td>$prcp</td></tr>";
+                }
+            ?>
         </table>
     </center>
 

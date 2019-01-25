c423de939e24619822afaffb84a0f3af1a3a8189
Kevin <kevinbeckwee@gmail.com>
2019-01-25 20:08:25 +0100

    Data uitlezen

--- DataGrafiek/Grafiek.php
+++ DataGrafiek/Grafiek.php
@@ -3,6 +3,21 @@
     <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?>
+<?php
+include_once("inc/dataReader.php");
+$station = readDataOfStation("2019-01-21",'100050' , "11000001000", 60, FALSE);
+//echo $station;
+/*foreach($station as $test){
+    print_r($test);
+    echo '<br/>';
+}*/
+for($i = 0; $i < count($station['time']); $i++){
+    $time = $station['time'][$i];
+    $temp = round($station['temp'][$i], 1);
+    $prcp = round($station['prcp'][$i], 2);
+    echo "<tr><th>$time</th><br/><td>$temp</td><br/><td>$prcp</td></tr>";
+}
+?>
+
 </body>
 </html>

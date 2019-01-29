84544b91c9c1e13d516c7ddf149e2bdd66670a99
Kevin <kevinbeckwee@gmail.com>
2019-01-29 03:41:42 +0100

    heatindexgraph aangepast

--- heatindexgraph.php
+++ heatindexgraph.php
@@ -1,40 +1,128 @@
 <html>
-    <head>
-        <title>Top 10 Heat Index - Hero Cycles Weather Application</title>
+<head>
+    <title>Top 10 Heat Index - Hero Cycles Weather Application</title>
+    <?php
+    require("inc/headermodule.php");
+    require("inc/loginrequire.php");
+    ?>
+</head>
+<?php include("inc/popup.php"); ?>
+<div id="center_content">
+    <?php
+    include_once("inc/dataReader.php");
+    $country = readDataOfCountry("2019-01-21", "INDIA", "110000000000", 60, FALSE);
+    ?>
+    <div class="Form_container" style="position: center">
+        <form action="#" method="post" style="text-align: center">
+            <select name="Station">
+                <?php
+                foreach ($country as $stationX) { ?>
+                    <option value="<?= $stationX['stn'] ?>"><?= $stationX['name'] ?></option>
+                    <?php
+                } ?>
+            </select>
+            <input type="submit" name="submit" value="Get Selected Values"/>
+        </form>
         <?php
-        require("inc/headermodule.php");
-        require("inc/loginrequire.php");
+        if (isset($_POST['submit'])) {
+            $selected_val = $_POST['Station'];  // Storing Selected Value In Variable
+            $station_number = $selected_val;
+            $selected_val = readDataOfStation("2019-01-21", $selected_val, "11100001000", 60, FALSE);  // Retrieving Selected Value
+        }
         ?>
-    </head>
-    <?php include ("inc/popup.php"); ?>
-    <div id="center_content">
-        <center>
-            <body>
-                <h1>HIER MOET EEN LIVE GRAFIEK KOMEN MET DE AANGEPASTE TEMPERATUUR OFWEL HEATINDEX , TABEL KAN ER UIT IS FILLER</h1>
-                <table>
-                    <tr><th></th><th>Weather Station</th><th>Country</th><th>Heat Index</th></tr>
-                    <tr><th>1</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>2</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>3</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>4</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>5</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>6</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>7</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>8</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>9</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                    <tr><th>10</th><td>1234567890.0987</td><td>India</td><td>27°C</td></tr>
-                </table>
-        </center>
     </div>
-    <div id="right_sidebar"></div>
 
-    <div id="footer">
-        <center>
-            <button></i> Download data to XML</button>
-        </center>
-        <?php
-        require("inc/footermodule.php")
-        ?>
+
+    <div class="chart_container" style="position: relative; height: auto; width: 100%">
+        <canvas id="myChart"></canvas>
     </div>
+
+    <script src="lib/Chart.bundle.min.js"></script>
+    <script>
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: [
+                        <?php $rtrn_array = array();
+                        foreach ($selected_val['time'] as $time) {
+                            $rtrn_array[] = '"' . formatSeconds($time) .
+                                '"';
+                        }
+                        echo implode(",", $rtrn_array);?>
+                    ],
+                    datasets: [{
+                        label: 'Weather data of India/station ' + <?php echo $station_number?>,
+                        data: [
+                            <?php
+                            $rtrn_array = array();
+                            $counter = 0;
+                            foreach ($selected_val['dewp'] as $temp) {
+                                $rtrn_array[] = '"' . floorp(calculateHeatIndex($selected_val['temp'][$counter], $temp), 2) . '"';
+                            }
+                            echo implode(",", $rtrn_array)
+                            ?>],
+                        backgroundColor: [
+                            'rgba(57, 106, 203, 0.5)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                    ,
+                    responsive: true
+                }
+            })
+        ;
+
+    </script>
+    <script>
+        var interval =
+            setInterval(function updateChart() {
+                    myChart.data.datasets[0].data =
+                        [
+                            <?php
+                            $rtrn_array = array();
+                            $counter = 0;
+                            foreach ($selected_val['dewp'] as $temp) {
+
+                                $rtrn_array[] = '"' . floorp(calculateHeatIndex($selected_val['temp'][$counter], $temp), 2) . '"';
+
+                            }
+                            echo implode(",", $rtrn_array)
+                            ?>];
+                    myChart.data.labels = [
+                        <?php $rtrn_array = array();
+                        foreach ($selected_val['time'] as $time) {
+                            $rtrn_array[] = '"' . formatSeconds($time) .
+                                '"';
+                        }
+                        echo implode(",", $rtrn_array);?>
+                    ];
+
+
+                    myChart.update();
+
+                }
+                ,
+                60000 //elke minuut update de chart
+            )
+    </script>
+</div>
 </body>
 </html>

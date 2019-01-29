be0a52ff5bee92f2aa42e35aa30f79945377cc51
Youri <Youri.sanders@hotmail.com>
2019-01-29 15:08:29 +0100

    Customer requirement quickly done

--- inc/toptentable.php
+++ inc/toptentable.php
@@ -64,6 +64,6 @@
     $highest_temperatures = array_slice($highest_temperatures, 0, 10, TRUE);
     $rank = 1;
     foreach ($highest_temperatures as $key => $value) {
-        echo "<tr><th>" . $rank++ . "</th><th>" . $key . "</th><th>" . $value[1] . "</th><th>" . $value[2] . "</th><th>" . round($value[0], 1) . "</th></tr>";
+        echo "<tr><th>Rank #" . $rank++ . "</th><th>" . $key . "</th><th>" . $value[1] . "</th><th>" . $value[2] . "</th><th>" . round($value[0], 1) . "°C</th></tr>";
     }
     ?>

fa2c1a82a7249063ab9acb78829790a8d5654861
Mike Clarke <mikec655@gmail.com>
2019-02-24 15:28:06 +0100

    changed error message

--- inc/toptentable.php
+++ inc/toptentable.php
@@ -6,19 +6,30 @@ include("countriesasia.php");
 $highest_temperatures = array();
 
 $data = readDataOfAsia(date("Y-m-d"), "110000000000", 60, FALSE);
+$missing_data = false;
 foreach ($data as $station) {
-    $highest_temperatures[$station[0]] = array(max($station[5]['temp']), $station[2], $station[1]);
+    if ($station[5] == -1){
+        $missing_data = true;
+    } else {
+        $highest_temperatures[$station[0]] = array(max($station[5]['temp']), $station[2], $station[1]);
+    }
 }
 
-arsort($highest_temperatures);
-$highest_temperatures = array_slice($highest_temperatures, 0, 10, TRUE);
-$rank = 1;
+if ($missing_data) {
+    echo "<center><p>";
+    echo "Top 10 could not be created because some data is missing";
+    echo "</center></p>";
+} else {
+    arsort($highest_temperatures);
+    $highest_temperatures = array_slice($highest_temperatures, 0, 10, TRUE);
+    $rank = 1;
 
-echo "<center><h1>Top 10 temperatures in Asia</h1>";
-echo "<table><tr><th></th><th>Weather Station</th><th>Country</th><th>Name</th><th>Temperature</th></tr>";
-foreach ($highest_temperatures as $key => $value) {
-    echo "<tr><th>Rank #" . $rank . "</th><th>" . $key . "</th><th>" . $value[1] . "</th><th>" . $value[2] . "</th><th>" . round($value[0], 1) . "°C</th></tr>";
-    $rank++;
+    echo "<center><h1>Top 10 temperatures in Asia</h1>";
+    echo "<table><tr><th></th><th>Weather Station</th><th>Country</th><th>Name</th><th>Temperature</th></tr>";
+    foreach ($highest_temperatures as $key => $value) {
+        echo "<tr><th>Rank #" . $rank . "</th><th>" . $key . "</th><th>" . $value[1] . "</th><th>" . $value[2] . "</th><th>" . round($value[0], 1) . "°C</th></tr>";
+        $rank++;
+    }
+    echo "</table></center>";
 }
-echo "</table></center>";
 ?>
--- inc/toptenxml.php
+++ inc/toptenxml.php
@@ -6,37 +6,44 @@ include("countriesasia.php");
 $highest_temperatures = array();
 
 $data = readDataOfAsia(date("Y-m-d"), "110000000000", 60, FALSE);
+$missing_data = false;
 foreach ($data as $station) {
-    $highest_temperatures[$station[0]] = array(max($station[5]['temp']), $station[2], $station[1]);
+    if ($station[5] == -1){
+        $missing_data = true;
+    } else {
+        $highest_temperatures[$station[0]] = array(max($station[5]['temp']), $station[2], $station[1]);
+    }
 }
 
-arsort($highest_temperatures);
-$highest_temperatures = array_slice($highest_temperatures, 0, 10, TRUE);
-$rank = 1;
+if (!$missing_data) {
+    arsort($highest_temperatures);
+    $highest_temperatures = array_slice($highest_temperatures, 0, 10, TRUE);
+    $rank = 1;
 
-$linkpath = 'Top 10 of ' . date("Y-m-d") . '.xml';
+    $linkpath = 'Top 10 of ' . date("Y-m-d") . '.xml';
 
-$dom = new DOMDocument();
-$dom->encoding = 'utf-8';
-$dom->xmlVersion = '1.0';
-$dom->formatOutput = true;
-$root = $dom->createElement('Top10_countries');
-foreach ($highest_temperatures as $key => $value) {
-    $station_node = $dom->createElement('Rank' . $rank);
-    $attr_rank_id = new DOMAttr('rank', $rank);
-    $station_node->setAttributeNode($attr_rank_id);
-    $child_node_stationnr = $dom->createElement('Station_number', $key);
-    $station_node->appendChild($child_node_stationnr);
-    $child_node_country = $dom->createElement('Country', $value[1]);
-    $station_node->appendChild($child_node_country);
-    $child_node_stationname = $dom->createElement('Station_Name', $value[2]);
-    $station_node->appendChild($child_node_stationname);
-    $child_node_temperature = $dom->createElement('Temp', round($value[0], 1));
-    $station_node->appendChild($child_node_temperature);
-    $root->appendChild($station_node);
-    $dom->appendChild($root);
-    $rank++;
+    $dom = new DOMDocument();
+    $dom->encoding = 'utf-8';
+    $dom->xmlVersion = '1.0';
+    $dom->formatOutput = true;
+    $root = $dom->createElement('Top10_countries');
+    foreach ($highest_temperatures as $key => $value) {
+        $station_node = $dom->createElement('Rank' . $rank);
+        $attr_rank_id = new DOMAttr('rank', $rank);
+        $station_node->setAttributeNode($attr_rank_id);
+        $child_node_stationnr = $dom->createElement('Station_number', $key);
+        $station_node->appendChild($child_node_stationnr);
+        $child_node_country = $dom->createElement('Country', $value[1]);
+        $station_node->appendChild($child_node_country);
+        $child_node_stationname = $dom->createElement('Station_Name', $value[2]);
+        $station_node->appendChild($child_node_stationname);
+        $child_node_temperature = $dom->createElement('Temp', round($value[0], 1));
+        $station_node->appendChild($child_node_temperature);
+        $root->appendChild($station_node);
+        $dom->appendChild($root);
+        $rank++;
+    }
+    $dom->save("../xml/" . $linkpath);
 }
-$dom->save("../xml/" . $linkpath);
 
 ?>
